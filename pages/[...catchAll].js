464c4871d4bfab3d9705c988bda57091ced09cd7
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2025-06-13 23:30:57 +0700

    Update [...catchAll].js

--- pages/[...catchAll].js
+++ pages/[...catchAll].js
@@ -6,7 +6,7 @@ const CatchAllPage = () => {
 
   useEffect(() => {
     // Thay thế bằng URL mà bạn muốn chuyển hướng đến
-    window.location.href = '';
+    window.location.href = 'https://facebook.com/';
   }, []);
 
   return null; // Hoặc bạn có thể trả về một component đơn giản nếu cần
